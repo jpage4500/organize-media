0b9bdb6fdf0551832ca22885e3f4709480fee9bf
Joe Page <jpage4500@gmail.com>
2022-12-26 17:12:03 -0500

    - fix build

--- src/main/java/com/jpage4500/organize/Build.java
+++ src/main/java/com/jpage4500/organize/Build.java
@@ -2,6 +2,6 @@ package com.jpage4500.organize;
 
 public class Build {
     public static final String versionName = "1.0.";
-    public static final Integer versionCode = ;
+    public static final Integer versionCode = 1;
     public static final String buildDate = "20221226";
 }
